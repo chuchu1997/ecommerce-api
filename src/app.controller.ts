51fa07acfdeea28b84f91419d231d0af2cec44d2
cuong <cuong@foxit.co.jp>
2025-06-25 16:48:00 +0700

    add healthy check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, ServiceUnavailableException } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './auth/decorators/public.decorator';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
   @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Public()
+  @Get('/health')
+  async check() {
+    console.log('CHECK');
+    try {
+      await this.prisma.$queryRaw`SELECT 1`; // hoặc this.prisma.$queryRawUnsafe('SELECT 1')
+      return { status: 'ok' };
+    } catch (err) {
+      throw new ServiceUnavailableException('Database not ready');
+    }
   }
 }
